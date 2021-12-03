fb452919724a4a6e785317269ac16721ff3964a6
Robin Norwood <rnorwood@hashicorp.com>
2021-12-03 15:02:12 -0600

    Updates for CDKTF 0.8 and latest AWS provider

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -2,8 +2,8 @@ import * as path from "path";
 import { Construct } from "constructs";
 import { App, TerraformStack, TerraformAsset, AssetType, TerraformOutput } from "cdktf";
 
-import * as aws from '@cdktf/provider-aws';
-import * as random from './.gen/providers/random';
+import * as aws from "@cdktf/provider-aws";
+import * as random from "./.gen/providers/random";
 
 interface LambdaFunctionConfig {
   path: string,
@@ -11,7 +11,7 @@ interface LambdaFunctionConfig {
   runtime: string,
   stageName: string,
   version: string,
-}
+};
 
 const lambdaRolePolicy = {
   "Version": "2012-10-17",
@@ -25,20 +25,22 @@ const lambdaRolePolicy = {
       "Sid": ""
     }
   ]
-}
+};
 
 class LambdaStack extends TerraformStack {
   constructor(scope: Construct, name: string, config: LambdaFunctionConfig) {
     super(scope, name);
 
-    new aws.AwsProvider(this, "provider", {
+    new aws.AwsProvider(this, "aws", {
       region: "us-west-2",
     });
+
+    new random.RandomProvider(this, "random");
 
     // Create random value
     const pet = new random.Pet(this, "random-name", {
       length: 2,
-    })
+    });
 
     // Create Lambda executable
     const asset = new TerraformAsset(this, "lambda-asset", {
@@ -47,31 +49,31 @@ class LambdaStack extends TerraformStack {
     });
 
     // Create unique S3 bucket that hosts Lambda executable
-    const bucket = new aws.S3Bucket(this, "bucket", {
+    const bucket = new aws.s3.S3Bucket(this, "bucket", {
       bucketPrefix: `learn-cdktf-${name}`,
     });
 
     // Upload Lambda zip file to newly created S3 bucket
-    const lambdaArchive = new aws.S3BucketObject(this, "lambda-archive", {
+    const lambdaArchive = new aws.s3.S3BucketObject(this, "lambda-archive", {
       bucket: bucket.bucket,
       key: `${config.version}/${asset.fileName}`,
       source: asset.path, // returns a posix path
     });
 
     // Create Lambda role
-    const role = new aws.IamRole(this, "lambda-exec", {
+    const role = new aws.iam.IamRole(this, "lambda-exec", {
       name: `learn-cdktf-${name}-${pet.id}`,
       assumeRolePolicy: JSON.stringify(lambdaRolePolicy)
-    })
+    });
 
     // Add execution role for lambda to write to CloudWatch logs
-    new aws.IamRolePolicyAttachment(this, "lambda-managed-policy", {
+    new aws.iam.IamRolePolicyAttachment(this, "lambda-managed-policy", {
       policyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
       role: role.name
-    })
+    });
 
     // Create Lambda function
-    const lambdaFunc = new aws.LambdaFunction(this, "learn-cdktf-lambda", {
+    const lambdaFunc = new aws.lambdafunction.LambdaFunction(this, "learn-cdktf-lambda", {
       functionName: `learn-cdktf-${name}-${pet.id}`,
       s3Bucket: bucket.bucket,
       s3Key: lambdaArchive.key,
@@ -81,32 +83,31 @@ class LambdaStack extends TerraformStack {
     });
 
     // Create and configure API gateway
-    const api = new aws.Apigatewayv2Api(this, "api-gw", {
+    const api = new aws.apigatewayv2.Apigatewayv2Api(this, "api-gw", {
       name: name,
       protocolType: "HTTP",
       target: lambdaFunc.arn
-    })
+    });
 
-    new aws.LambdaPermission(this, "apigw-lambda", {
+    new aws.lambdafunction.LambdaPermission(this, "apigw-lambda", {
       functionName: lambdaFunc.functionName,
       action: "lambda:InvokeFunction",
       principal: "apigateway.amazonaws.com",
       sourceArn: `${api.executionArn}/*/*`,
-    })
+    });
 
     new TerraformOutput(this, 'url', {
       value: api.apiEndpoint
     });
-
   }
-}
+};
 
 const app = new App();
 
 new LambdaStack(app, 'lambda-hello-world', {
   path: "../lambda-hello-world/dist",
   handler: "index.handler",
-  runtime: "nodejs10.x",
+  runtime: "nodejs14.x",
   stageName: "hello-world",
   version: "v0.0.2"
 });
@@ -114,7 +115,7 @@ new LambdaStack(app, 'lambda-hello-world', {
 new LambdaStack(app, 'lambda-hello-name', {
   path: "../lambda-hello-name/dist",
   handler: "index.handler",
-  runtime: "nodejs10.x",
+  runtime: "nodejs14.x",
   stageName: "hello-name",
   version: "v0.0.1"
 });
